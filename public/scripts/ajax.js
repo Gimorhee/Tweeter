050c7cb0ed339411e819ed988fbfb31da5571838
Danny <dongyunrhee@gmail.com>
2019-04-17 01:00:25 +0000

    Forming data into a query string by using .serialize() function.

--- public/scripts/ajax.js
+++ public/scripts/ajax.js
@@ -3,5 +3,11 @@ $(function() {
   $form.on('submit', function () {
     event.preventDefault();
     console.log("Button works");
+    let $formData = $form.serialize();
+    console.log($formData);
+    $.ajax('/tweets', { method: 'POST', data: $formData })
+    .then(function (event) {
+      console.log("Success:", event);
+    });
   });
 });
