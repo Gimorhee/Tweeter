3b5edcb9f11eac1fc5b380d601a27f549cb67c16
Danny <dongyunrhee@gmail.com>
2019-04-17 02:51:30 +0000

    Form validation. Alerting error message when there is no input or the input is too long.

--- public/scripts/ajax.js
+++ public/scripts/ajax.js
@@ -2,12 +2,20 @@ $(function() {
   let $form = $('#tweet-form');
   $form.on('submit', function () {
     event.preventDefault();
-    console.log("Button works");
-    let $formData = $form.serialize();
-    console.log($formData);
-    $.ajax('/tweets', { method: 'POST', data: $formData })
-    .then(function (event) {
-      console.log("Success:", event);
-    });
+    let inputLength = $("#tweet-input").val().length;
+    if(inputLength === 0 || inputLength === null) {
+      alert("Error: 0 ");
+    } else if (inputLength > 140) {
+      alert("Error: > 140");
+    } else {
+      console.log("Button works");
+      let $formData = $form.serialize();
+      console.log($formData);
+      $.ajax('/tweets', { method: 'POST', data: $formData })
+      .then(function (event) {
+        console.log("Success:", event);
+      });
+    }
   });
 });
+
