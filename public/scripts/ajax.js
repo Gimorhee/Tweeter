f0c77088a671d8fabb5d1a5f6b660386e35b87e2
Danny <dongyunrhee@gmail.com>
2019-04-17 16:42:27 +0000

    Editing some codes (ES6)

--- public/scripts/ajax.js
+++ public/scripts/ajax.js
@@ -1,21 +1,27 @@
-$(function() {
+$(() => {
   let $form = $('#tweet-form');
-  $form.on('submit', function () {
+  $form.on('submit', () => {
     event.preventDefault();
     let inputLength = $("#tweet-input").val().length;
     if(inputLength === 0 || inputLength === null) {
-      alert("Error: 0 ");
+      alert("Invalid input. Tweet must not be empty. Please try again.");
     } else if (inputLength > 140) {
-      alert("Error: > 140");
+      alert("Your tweet is too long! Please shorten it and try again.");
     } else {
       console.log("Button works");
       let $formData = $form.serialize();
       console.log($formData);
-      $.ajax('/tweets', { method: 'POST', data: $formData })
-      .then(function (event) {
-        console.log("Success:", event);
+      $.ajax({
+        url: '/tweets',
+        method: 'POST',
+        data: $formData,
+        success: data => {
+          console.log("Success:", data);
+        },
+        failure: error => {
+          console.log(error);
+        }
       });
     }
   });
 });
-
