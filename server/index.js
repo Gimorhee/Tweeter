8870edb3be74a04c55871136348ab5a24d728a56
Danny <dongyunrhee@gmail.com>
2019-04-19 20:33:27 +0000

    Fix mongo connection string issue

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 "use strict";
 
-const PORT          = 8080;
+const PORT          = process.env.PORT || 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
